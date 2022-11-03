9331f2ae7e9b24f939b97ab9f706f11b9691d3ea
unknown <antoniocrespgome@gmail.com>
2022-11-03 14:09:30 +0100

    .

---
+++ AccesoDatos/Main.java
@@ -0,0 +1,133 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		boolean seleccionado=false;
+		String ruta="";
+		
+		//Con el fileChooser obtenemos la ruta del fichero a leer
+		Scanner entrada = null;
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.showOpenDialog(fileChooser);
+		try{
+			ruta =fileChooser.getSelectedFile().getAbsolutePath();
+			seleccionado=true;
+		}catch(NullPointerException e){
+			System.err.println("No se ha seleccionado ningún fichero");
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}finally{
+			if(entrada !=null){
+				entrada.close();
+			}
+		}
+		
+		RandomAccessFile fichero=null;
+		
+		
+		if(seleccionado) {
+			try {
+				fichero=new RandomAccessFile(ruta,"rw");
+			} catch (FileNotFoundException e) {
+				System.err.println(e.getMessage());
+			}
+			
+		}
+		
+		boolean repeat=false;
+		do {
+		repeat=true;
+		int seleccion = JOptionPane.showOptionDialog(null,"Seleccione opcion", "Selector de opciones",
+		JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null, 
+		new Object[] { "añadir double al principio", "añadir double al final", "mostrar fichero", "sustituir un nº por otro", "salir" },"opcion 1");
+		switch(seleccion) {
+		case 0:doublePrincipio(fichero);
+			break;
+		case 1:doubleFinal(fichero);
+			break;
+		case 2:mostrarFichero(fichero);
+			break;
+		case 3:sustituirNum(fichero);
+			break;
+		default:repeat=false;
+			break;
+		}
+		}while(repeat);
+	}
+	
+	private static void doublePrincipio(RandomAccessFile fichero) {
+		boolean repeat=false;
+		double d1=0;
+		
+		do {
+		repeat=false;
+		String seleccion = JOptionPane.showInputDialog(null,"Escribe un numero double",JOptionPane.QUESTION_MESSAGE);
+		try {
+			d1=Double.parseDouble(seleccion);
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null,"Eso no es un double desgraciado");
+			repeat=true;
+		}
+		}while(repeat);
+		
+		try {
+			fichero.seek(0);
+			String aux=d1+"";
+			fichero.writeBytes(aux);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}	
+	}
+	
+	private static void doubleFinal(RandomAccessFile fichero) {
+		boolean repeat=false;
+		double d1=0;
+		
+		do {
+		repeat=false;
+		String seleccion = JOptionPane.showInputDialog(null,"Escribe un numero double",JOptionPane.QUESTION_MESSAGE);
+		try {
+			d1=Double.parseDouble(seleccion);
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null,"Eso no es un double desgraciado");
+			repeat=true;
+		}
+		}while(repeat);
+		
+		try {
+			fichero.seek(fichero.length());
+			String aux=d1+"";
+			fichero.writeBytes(aux);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	private static void mostrarFichero(RandomAccessFile fichero) {
+		String show="";
+		try {
+			fichero.seek(0);
+			String linea=fichero.readLine();
+			while (linea!=null) {
+				show+=linea+"\n";
+				linea=fichero.readLine();
+			}
+		}catch(IOException e) {
+			System.err.println(e.getMessage());
+		};
+		JOptionPane.showMessageDialog(null,show);
+	}
+	
+	private static void sustituirNum(RandomAccessFile fichero) {
+		
+	}
+
+}
